2836a8a64cc5ea8f2d1e6fad4020dc00ec02337d
mingjin0101 <mingjin0101@users.noreply.github.com>
2024-05-12 21:52:37 +0800

    加入專案檔案。

---
+++ Homework07/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Homework07
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.InactiveCaption;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Microsoft YaHei UI", 30F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.MenuHighlight;
+            button1.Location = new Point(295, 278);
+            button1.Name = "button1";
+            button1.Size = new Size(178, 74);
+            button1.TabIndex = 0;
+            button1.Text = "Start";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BorderStyle = BorderStyle.Fixed3D;
+            label1.FlatStyle = FlatStyle.Flat;
+            label1.Font = new Font("Tw Cen MT Condensed Extra Bold", 50F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ActiveCaptionText;
+            label1.Location = new Point(295, 115);
+            label1.Name = "label1";
+            label1.Size = new Size(194, 101);
+            label1.TabIndex = 1;
+            label1.Text = "Time";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private Label label1;
+    }
+}
---
+++ Homework07/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+
+namespace Homework07
+{
+    public partial class Form1 : Form
+    {
+        private int count = 100; // Initialize the count variable with a value of 100
+        private Timer timer = new Timer();  // Declare a Timer variable
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer = new Timer(); // Create a new instance of Timer
+            timer.Interval = 1000; // Set the interval of the timer to 1000 milliseconds (1 second)
+            timer.Tick += timer1_Tick; // Attach the timer_Tick event handler to the Tick event of the timer
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            count--; // Decrement the count variable by 1
+            label1.Text = count.ToString(); // Update the text of the label with the current count value
+            if (count <= 0)
+            {
+                timer.Stop(); // Stop the timer
+                MessageBox.Show("時間到!"); // Display a message box indicating that time is up
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer.Start(); // Start the timer
+            timer.Interval = 1000;
+        }
+
+        
+    }
+}
